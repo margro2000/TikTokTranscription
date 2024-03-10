c658bbee4cfd41ffe086299c6ccaaf004019d821
Aditya Advani <aditya@bestparents.com>
2024-03-09 19:52:13 -0800

    feat: works great

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import json
-from utils.anthropic_helpers import get_message_from_anthropic
+from utils.openai_helpers import get_openai_response
 
 def get_json_from_response(text):
 
@@ -21,11 +21,56 @@ def generate_topics_from_module_title(module_title):
     prompt = f"""
     Given the following module title "{module_title}", generate a list of topics that could be covered in the module.
     
-    Along with each topic, I need a search query for tiktok. Try to stick to 5 or less topics.
+    Along with each topic, I need a search query for tiktok.
     
-    The way our app works, we are finding 1 tiktok video per topic to teach that topic. 
+    The way our app works, we are finding 1 tiktok video per topic to teach that topic. Try to stick to no more than 5 topics please.
     
-    The video should be engaging yet comprehensive.
+    The video should be engaging yet comprehensive. 
+    
+    For example, given the module title "Working with objects using Adobe Illustrator", we expect output like this, except whittled down to 5.
+    
+    [
+        {{
+            "topic": "Creating shapes and objects",
+            "search_query": "adobe illustrator creating shapes objects tutorial"
+        }},
+        {{
+            "topic": "Selecting and grouping objects",
+            "search_query": "adobe illustrator selecting grouping objects tips"
+        }},
+        {{
+            "topic": "Transforming objects (resize, rotate, reflect)",
+            "search_query": "adobe illustrator transform objects tutorial"
+        }},
+        {{
+            "topic": "Aligning and distributing objects",
+            "search_query": "adobe illustrator align distribute objects guide"
+        }},
+        {{
+            "topic": "Using the Pathfinder panel",
+            "search_query": "adobe illustrator pathfinder panel tutorial"
+        }},
+        {{
+            "topic": "Working with layers and stacking order",
+            "search_query": "adobe illustrator layers stacking order tips"
+        }},
+        {{
+            "topic": "Masking and clipping objects",
+            "search_query": "adobe illustrator masking clipping objects tutorial"
+        }},
+        {{
+            "topic": "Blending and merging objects",
+            "search_query": "adobe illustrator blend merge objects guide"
+        }},
+        {{
+            "topic": "Using the Shape Builder tool",
+            "search_query": "adobe illustrator shape builder tool tutorial"
+        }},
+        {{
+            "topic": "Organizing objects with groups and layers",
+            "search_query": "adobe illustrator organize objects groups layers tips"
+        }}
+    ]
     
     Please return the list of topics and search queries in the following format:
 
@@ -44,6 +89,6 @@ def generate_topics_from_module_title(module_title):
     """
     # print(prompt)
 
-    text = get_message_from_anthropic(prompt)
+    text = get_openai_response(prompt)
     response = get_json_from_response(text)
     return response
